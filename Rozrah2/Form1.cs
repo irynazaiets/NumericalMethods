418dd3d881e86631c5c04bb6fc24aea914adf9e6
Iryna Zaiets <irynazaiets98@ukr.net>
2019-06-02 23:12:32 +0300

    First commit

---
+++ Rozrah2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rozrah2
+{
+    public partial class Form1 : Form
+    {
+        public Form1( )
+        {
+            InitializeComponent( );
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            dataGridView1.Rows.Clear( );
+            var interp = new Interpolation( );
+            var tablevalues = new double[2, 6] { { 0.0, Math.PI / 24, Math.PI / 20, Math.PI / 16, Math.PI / 12, Math.PI / 8 }, 
+                {interp.CalcFunctionInPoint(0.0), interp.CalcFunctionInPoint(Math.PI / 24), interp.CalcFunctionInPoint(Math.PI / 20), 
+                    interp.CalcFunctionInPoint(Math.PI / 16), interp.CalcFunctionInPoint(Math.PI / 12), interp.CalcFunctionInPoint(Math.PI / 8) } };
+
+            myChart.Series[0].Color = Color.Red;
+            for( int i = 0; i < 6; ++i )
+            {
+                myChart.Series[0].Points.AddXY( tablevalues[0, i], tablevalues[1, i] );
+                myChart.Series[1].Points.AddXY( tablevalues[0, i], tablevalues[1, i] );
+                dataGridView1.Rows.Add( tablevalues[0, i], tablevalues[1, i] );
+            }
+
+            interp.CreateTable( tablevalues , 6 , 7 );
+            mnogochlen.Text = interp.FormMnogochlen( 6 );
+            var x_1 = interp.CalcPolynomialInPoint( 0.1 );
+            var x_2 = interp.CalcPolynomialInPoint( 0.2 );
+            var x_3 = interp.CalcPolynomialInPoint( 0.3 );
+            var x_4 = interp.CalcPolynomialInPoint( Math.PI / 10 );
+
+
+            x1.Text = "L(x`) = L(0.1) = " + x_1 +"\n";
+            x1.Text += "L(x``) = L(0.2) = " + x_2 + "\n";
+            x1.Text += "L(x```) = L(0.3) = " + x_3 + "\n";
+            x1.Text += "L(x) = L(Pi/10) = " + x_4 + "\n";
+            
+            var interpTable = interp.InterpValue;
+            OutputInterp( interpTable, 6, 7 );
+
+            dataGridView2.Rows.Add( 0.1, interp.CalcDeviation(0.1, x_1 ));
+            dataGridView2.Rows.Add( 0.2, interp.CalcDeviation( 0.2, x_2 ) );
+            dataGridView2.Rows.Add( 0.3, interp.CalcDeviation( 0.3, x_3 ) );
+            dataGridView2.Rows.Add( Math.PI/10, interp.CalcDeviation( Math.PI / 10, x_4 ) );
+
+        }
+
+        #region MyCustomOutput
+        public void OutputInterp( double[,] matrix, double countOfRow, int countOfCol )
+        {
+            label5.Text =  "Table of divided differences:\n";
+            for( int e = 0; e < countOfRow; ++e )
+            {
+                for( int w = 0; w < countOfCol; ++w )
+                {
+                    if( e > 0 && w>1 && matrix[e, w] == 0 )
+                    {
+                        label5.Text += "         " ;
+                    } else
+                    {
+                        if( matrix[e, w] >= 0 )
+                            label5.Text += String.Format( " {0:#,##0.00000}", matrix[e, w] ) + "  " ;
+                        else
+                            label5.Text += String.Format( "{0:#,##0.00000}", matrix[e, w] ) + "  " ;
+                    }
+                }
+                label5.Text += "\n";
+            }
+        }
+        #endregion
+        private void Form1_Load( object sender, EventArgs e )
+        {
+
+        }
+    }
+}
---
+++ Rozrah2/Interpolation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozrah2
+{
+    public class Interpolation
+    {
+        private double[,] _interpValue;
+        private int _rowCount;
+
+        public double[,] InterpValue {
+            get { return _interpValue; } 
+            set { _interpValue = value; }
+        } 
+        
+        public Interpolation( ) {
+            InterpValue = new double[6, 10];
+        }
+
+        public void CreateTable( double[,] matrix, int rowCount, int columnCount ) {
+            _rowCount = rowCount;
+            for( int i = 0; i < _rowCount; ++i ) {
+                InterpValue[i, 0] = matrix[0, i];
+                InterpValue[i, 1] = matrix[1, i];
+            }
+
+            for( int i = 1; i < _rowCount; ++i ) {
+                for( int j = 0; j < _rowCount - i; ++j ) {
+
+                    //Знаходимо розділені різниці
+
+                    var newElem = ( InterpValue[j, i] - InterpValue[j + 1, i] ) / ( InterpValue[j, 0] - InterpValue[j + i, 0] );
+                    InterpValue[j, i + 1] = newElem;
+                }
+            }
+        }
+
+        //Обчислення значення інтерполяційного многочлена в заданій точці, підставивши попередньо отримані значення розділених різниць
+
+        public double CalcPolynomialInPoint( double dot ) {
+            double L_horn = InterpValue[0, 6];
+            for( int j = 6 - 2; j >= 0; --j )
+            {
+                L_horn = InterpValue[0, j + 1] + ( dot - InterpValue[j, 0] ) * L_horn;
+            }
+
+            /*
+            double[] mnognyk = new double[_rowCount];
+            mnognyk[0] = 1;
+
+            for( int i = 1; i < _rowCount; ++i )
+            {
+                mnognyk[i] = mnognyk[i - 1] * ( dot - InterpValue[i - 1, 0] );
+            }
+            var L = 0.0;
+            for( int i = 0; i < _rowCount; ++i )
+            {
+                L += InterpValue[0, i + 1] * mnognyk[i];
+            }
+            */
+            return L_horn;
+        }
+
+        //Обчислення значення функції в заданій точці 
+
+        public double CalcFunctionInPoint( double dot )
+        {
+            return  Math.Sin(3 * dot)/24 + Math.Pow(dot, 5)/64;
+        }
+
+        //Обчислюємо похибки інтерполяції в точках
+
+        public double CalcDeviation(double dot, double polinomialValue )
+        {
+            return Math.Abs( CalcFunctionInPoint( dot ) - polinomialValue );
+        }
+
+        public string FormMnogochlen( int n ) {
+        string anotherStr = Math.Round( InterpValue[0, 6],5).ToString();
+        
+        //формуємо інтерполяційний многочлен
+        
+        for(int j = n-2; j>=0;--j )
+        {
+            var s = $"({anotherStr})";
+            anotherStr = InterpValue[j, 0]>= 0? InterpValue[j, 0] == 0 ? $"{Math.Round( InterpValue[0,j+1],5)}+x" : $"{Math.Round( InterpValue[0, j + 1],5)}+(x-{Math.Round( InterpValue[j, 0],5)})" 
+                                                : $"{Math.Round( InterpValue[0, j + 1],5)})+(x+{Math.Round( InterpValue[j, 0],5)})";
+            anotherStr += s;
+        }
+
+        return "Interpolation polynomial: \nL = " + anotherStr;
+        }
+    }
+}
